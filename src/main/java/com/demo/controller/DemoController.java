74780990d42d0fd2ec9af641db00e951b2fb58c2
Joe Zou <yixian.zou@gmail.com>
2018-04-07 09:11:44 +0800

    add log

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -1,7 +1,7 @@
 package com.demo.controller;
 
-import org.jboss.logging.Param;
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("prefix")
 public class DemoController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/getUserName")
     public String getUserName(@RequestParam("userId")String userId){
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
         return userId;
     }
 }
